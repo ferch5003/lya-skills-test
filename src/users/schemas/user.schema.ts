047e19e03b2a80d29affaa29c674776e0dbc0649
Fernando David Visbal Piedrahita <ferch5003@gmail.com>
2021-04-07 20:35:37 -0500

    added: Added hash passwor dfunctionality

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,11 +1,13 @@
 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export type UserDocument = User & Document;
+const SALT = 10;
 
 @Schema()
-export class User {
+export class User extends Document {
   @Prop()
   firstname: string;
 
@@ -25,6 +27,24 @@ export class User {
     default: false
   })
   active: boolean;
+
+  validatePassword: Function;
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre<User>('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    this.password = bcrypt.hashSync(this.password, SALT);
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+
+UserSchema.methods.validatePassword = async function (password: string): Promise<boolean> {
+  return bcrypt.compareSync(password, this.password);
+};
+
+export { UserSchema };
